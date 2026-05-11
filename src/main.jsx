48c76b266e039686ed08fb681b2d904a9143dd45
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2026-05-11 20:49:44 +0530

    Fix: move Bootstrap JS to CDN script tag, remove from Vite bundle

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
